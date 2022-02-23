644acb1aebad16df26215c3a383529a9dcfd4873
quoc2401 <quoc2401@gmail.com>
2022-02-23 10:02:38 +0700

    test them nhanh

--- Ca2KTPM/src/main/java/com/mycompany/ca2ktpm/PrimaryController.java
+++ Ca2KTPM/src/main/java/com/mycompany/ca2ktpm/PrimaryController.java
@@ -1,6 +1,7 @@
 package com.mycompany.ca2ktpm;
 
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +9,9 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    
+    private void checkHandler(EventHandler event) {
+        
     }
 }
